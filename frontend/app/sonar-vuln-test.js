22ce63b9470e8c6a9158e4e9e2d67fbc9dac925f
msj6785 <msj6785@naver.com>
2025-11-19 18:13:34 +0900

    real-20

--- frontend/app/sonar-vuln-test.js
+++ frontend/app/sonar-vuln-test.js
@@ -1,66 +1,66 @@
-// 테스트 용도로 SonarQube Code Smell을 유발하는 파일입니다.
-// 사용 후 반드시 삭제하세요.
+// // 테스트 용도로 SonarQube Code Smell을 유발하는 파일입니다.
+// // 사용 후 반드시 삭제하세요.
 
-function smell1() {
-  var unusedVar1;
-}
+// function smell1() {
+//   var unusedVar1;
+// }
 
-function smell2() {
-  var unusedVar2;
-}
+// function smell2() {
+//   var unusedVar2;
+// }
 
-function smell3() {
-  var unusedVar3;
-}
+// function smell3() {
+//   var unusedVar3;
+// }
 
-function smell4() {
-  var unusedVar4;
-}
+// function smell4() {
+//   var unusedVar4;
+// }
 
-function smell5() {
-  var unusedVar5;
-}
+// function smell5() {
+//   var unusedVar5;
+// }
 
-function smell6() {
-  var unusedVar6;
-}
+// function smell6() {
+//   var unusedVar6;
+// }
 
-function smell7() {
-  var unusedVar7;
-}
+// function smell7() {
+//   var unusedVar7;
+// }
 
-function smell8() {
-  var unusedVar8;
-}
+// function smell8() {
+//   var unusedVar8;
+// }
 
-function smell9() {
-  var unusedVar9;
-}
+// function smell9() {
+//   var unusedVar9;
+// }
 
-function smell10() {
-  var unusedVar10;
-}
+// function smell10() {
+//   var unusedVar10;
+// }
 
-function smell11() {
-  var unusedVar11;
-}
+// function smell11() {
+//   var unusedVar11;
+// }
 
-function smell12() {
-  var unusedVar12;
-}
+// function smell12() {
+//   var unusedVar12;
+// }
 
-function smell13() {
-  var unusedVar13;
-}
+// function smell13() {
+//   var unusedVar13;
+// }
 
-function smell14() {
-  var unusedVar14;
-}
+// function smell14() {
+//   var unusedVar14;
+// }
 
-function smell15() {
-  var unusedVar15;
-}
+// function smell15() {
+//   var unusedVar15;
+// }
 
-function smell16() {
-  var unusedVar16;
-}
+// function smell16() {
+//   var unusedVar16;
+// }
